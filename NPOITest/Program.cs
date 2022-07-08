4f61691d018a6683c1078724e6ece9935f691a2f
muzhanjie <3196434334@qq.com>
2022-07-08 10:31:48 +0800

    add NPOITest Example😀
    Signed-off-by: muzhanjie <3196434334@qq.com>

---
+++ NPOITest/Program.cs
@@ -0,0 +1,34 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"f:\test.xlsx";
+
+            //HSSFWorkbook workbook;
+            XSSFWorkbook workbook;
+            using (FileStream stream = File.OpenRead(fileName))
+                //workbook = new HSSFWorkbook(stream);
+                workbook = new XSSFWorkbook(stream);
+
+            ISheet sheet = workbook.CreateSheet("菜鸟4");
+            IRow row = sheet.CreateRow(0);
+            ICell cell = row.CreateCell(0);
+            cell.SetCellValue($"第0行 第0列");
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                workbook.Write(fs);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # component-Example-Csharp
 C#组件使用示例库
+
+NPOITest 表格组件NPOI使用示例
