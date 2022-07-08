969da2e7b3848c5755d5c1eb2401ea3a0602af06
muzhanjie <3196434334@qq.com>
2022-07-08 11:42:36 +0800

    add RegularNumberInString Example
    Signed-off-by: muzhanjie <3196434334@qq.com>

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 C#组件使用示例库
 
 NPOITest 表格组件NPOI使用示例
+RegularNumberInString 正则表达式提取字符串中的数字示例
---
+++ RegularNumberInString/RegularNumberInString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularNumberInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "提取123abc提取";    //我们抓取当前字符当中的123
+            string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
