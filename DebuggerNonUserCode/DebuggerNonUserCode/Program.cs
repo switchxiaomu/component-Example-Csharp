31c7e2724f78bd282ccc55b86ffe4ca33f2f5a83
muzhanjie <3196434334@qq.com>
2022-07-19 09:50:01 +0800

    add DebuggerNonUserCode 测试DebuggerNonUserCode属性的影响，避免断点调试的时候进入你不感兴趣的代码块中
    Signed-off-by: muzhanjie <3196434334@qq.com>

---
+++ DebuggerNonUserCode/DebuggerNonUserCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggerNonUserCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foo();
+            Console.ReadKey();
+        }
+
+        private static void foo()
+        {
+            Console.WriteLine("Hello");
+            boo();
+        }
+
+        //DebuggerNonUserCode属性为了避免断点调试进入你不感兴趣的代码块中
+        [DebuggerNonUserCode]
+        private static void boo()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
